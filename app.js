b436ee9ec9032617c45a0f6f751b248ad5e0e22a
aryan-098max <aryangupta4279@gmail.com>
2023-06-11 19:29:45 +0530

    second changes

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const mongoose = require("mongoose");
-const Portfolio = require("./models/portfolio");
 
 // sending mail
 const nodemailer = require("nodemailer");
